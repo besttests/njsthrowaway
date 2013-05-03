3ae28e4b875f7a0288ebb9d99717c2e064861ab7
Chris Halbersma <chalbersma.12@gmail.com>
2013-05-02 20:24:59 -0500

    Initial Helloworld
    Helloworld application

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var server = htttp.createServer(function(request, response) {
+    response.writeHead(200, {
+        // headers here
+        'Content-type' : 'text/plain'
+    });
+    response.write('Hello World');
+    response.end();
+});
+
+server.listen(80);
+
+console.log('Listening on http://localhost:80');
